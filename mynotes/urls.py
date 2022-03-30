4742816412d176764f3a4d0ab0e009985aebdef7
Daniel Pacek <d.pacek1@gmail.com>
2022-03-30 12:11:04 +0200

    changed path for api

--- mynotes/urls.py
+++ mynotes/urls.py
@@ -20,6 +20,5 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('api.urls')),
-    path('api-auth/', include('rest_framework.urls')),
     path('', TemplateView.as_view(template_name='index.html'), name='main'),
 ]
