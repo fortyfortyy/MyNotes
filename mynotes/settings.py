30f1d7bfce50e19958f63176b9807455a4d26525
Daniel Pacek <d.pacek1@gmail.com>
2022-04-06 19:19:35 +0200

    added token expire time for password reset

--- mynotes/settings.py
+++ mynotes/settings.py
@@ -31,6 +31,7 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+PASSWORD_RESET_TIMEOUT = 120  # reset password token after 2 min
 
 # Application definition
 
