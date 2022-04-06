c7154a69cf74b93518d6c767cc0a513900b5490a
Daniel Pacek <d.pacek1@gmail.com>
2022-04-06 14:27:51 +0200

    created template for activate user account

---
+++ frontend/src/pages/ActivateAccount.js
@@ -0,0 +1,28 @@
+import {
+    DivButton,
+    Form,
+    ActivateButton,
+    Container,
+    Title,
+} from "./styles/ActivateAccountStyles";
+
+import React, {useContext} from "react";
+import AuthContext from "../context/AuthContext";
+
+const ActivateAccount = () => {
+    let {activateAccount} = useContext(AuthContext)
+
+    return (
+        <Container>
+            <Title>Please click the button to activate your account</Title>
+            <Form>
+                <form method='POST' onSubmit={activateAccount}>
+                    <DivButton>
+                        <ActivateButton type="submit">Activate</ActivateButton>
+                    </DivButton>
+                </form>
+            </Form>
+        </Container>
+    )
+}
+export default ActivateAccount;
