9c928400c90338f730a218491ea1f17dfae2a895
Daniel Pacek <d.pacek1@gmail.com>
2022-04-06 14:23:47 +0200

    created validate func to check if user is active

--- users/serializers.py
+++ users/serializers.py
@@ -1,9 +1,14 @@
-from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework import serializers
+from rest_framework import status
+from rest_framework_simplejwt.exceptions import AuthenticationFailed
+from rest_framework_simplejwt.serializers import  TokenObtainSerializer
+from rest_framework_simplejwt.settings import api_settings
 
 from django.core import exceptions
 from django.core.validators import validate_email
+from django.contrib.auth.models import update_last_login
 from django.utils.translation import gettext_lazy as _
+from rest_framework_simplejwt.tokens import RefreshToken
 
 from api.models import ProfileUser
 
@@ -39,20 +44,38 @@ class RegisterSerializer(serializers.ModelSerializer):
         fields = ('email', 'password', 'first_name', 'password2')
 
 
-class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
-    """
-    Update the access token with the user's first_name value
-    """
+class InActiveUser(AuthenticationFailed):
+    status_code = status.HTTP_406_NOT_ACCEPTABLE
+    default_detail = "User is not active, please confirm your email"
+    default_code = 'user_is_inactive'
+
+
+class CustomTokenObtainPairSerializer(TokenObtainSerializer):
 
     @classmethod
     def get_token(cls, user):
-        token = super().get_token(user)
-
+        token = RefreshToken.for_user(user)
         # Add custom claims
         token['name'] = user.first_name
         # ...
 
         return token
+
+    def validate(self, attrs):
+        data = super().validate(attrs)
+
+        if not self.user.is_active:
+            raise InActiveUser()
+
+        refresh = self.get_token(self.user)
+
+        data['refresh'] = str(refresh)
+        data['access'] = str(refresh.access_token)
+
+        if api_settings.UPDATE_LAST_LOGIN:
+            update_last_login(None, self.user)
+
+        return data
 
 
 class ForgottenPasswordSerializer(serializers.Serializer):
