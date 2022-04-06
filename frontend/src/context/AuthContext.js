84e16d26a7bafa3489ab31e82c2d7608e8116f0d
Daniel Pacek <d.pacek1@gmail.com>
2022-04-06 19:27:08 +0200

    created pop ups for specific response from the server

--- frontend/src/context/AuthContext.js
+++ frontend/src/context/AuthContext.js
@@ -1,6 +1,7 @@
 import {createContext, useState, useEffect} from "react";
 import jwt_decode from "jwt-decode";
-import {Redirect, useHistory, useParams} from "react-router-dom";
+import {useHistory, useParams} from "react-router-dom";
+import {toast} from 'react-toastify';
 
 const AuthContext = createContext()
 export default AuthContext;
@@ -18,10 +19,73 @@ export const AuthProvider = ({children}) => {
     const history = useHistory() // can't use Redirect, instead need to use that method
     const params = useParams()
 
-    // async function wait on some network calls here and passing the event
+
+    let registerUser = async (e) => {
+        e.preventDefault()
+        let response = await fetch('http://127.0.0.1:8000/account/api/register/', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                'first_name': e.target.first_name.value,
+                'email': e.target.email.value,
+                'password': e.target.password.value,
+                'password2': e.target.password2.value,
+            })
+        })
+        let data = await response.json()
+        if (response.status === 201) {
+            toast.success("Account has been created!", {
+                position: toast.POSITION.TOP_RIGHT,
+            })
+            toast.warning("Please confirm your email to activate account", {
+                position: toast.POSITION.TOP_RIGHT,
+            })
+            setTimeout(function () {
+                window.location.replace('/login');
+            }, 7000);
+
+        } else {
+            toast.error('Something gone wrong, check your form', {
+                position: toast.POSITION.TOP_CENTER,
+            })
+        }
+    }
+
+    let activateAccount = async (e) => {
+        e.preventDefault()
+        let url = window.location.pathname
+        let url_split = url.split('/')
+        let uid = url_split[3]
+        let token = url_split[4]
+
+        let response = await fetch(`http://127.0.0.1:8000/account/activate/${uid}/${token}/`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+        let data = await response.json()
+        if (response.status === 200) {
+            toast.success("Your account has been activated!", {
+                position: toast.POSITION.TOP_RIGHT,
+                containerId: 'loginPage',
+            })
+
+        } else if (response.status === 400) {
+            toast.error('Link is invalid', {
+                position: toast.POSITION.TOP_CENTER,
+                containerId: 'loginPage',
+            })
+        }
+        history.push('/')
+    }
+
+
     let loginUser = async (e) => {
         e.preventDefault()
-        let response = await fetch('http://127.0.0.1:8000/api/token/', {
+        let response = await fetch('http://127.0.0.1:8000/account/api/token/', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
@@ -30,7 +94,6 @@ export const AuthProvider = ({children}) => {
         })
         let data = await response.json()
         if (response.status === 200) {
-
             // setAuthTokens value and store it in state and in local storage
             setAuthTokens(data)
 
@@ -39,16 +102,23 @@ export const AuthProvider = ({children}) => {
 
             // Set localstorage user's access token, whenever user comes back e.g next day, still can be logged in
             localStorage.setItem('authTokens', JSON.stringify(data))
+            toast.success("Hey 👋 you are logged in!", {
+                position: toast.POSITION.TOP_RIGHT,
+                containerId: 'loginPage',
+            })
             history.push('/') // redirect user to homepage
         } else {
-            console.log(response.status)
+            toast.error(data['detail'], {
+                position: "top-center",
+                autoClose: 3000,
+            });
         }
     }
 
 
     let resetPasswordUser = async (e) => {
         e.preventDefault()
-        let response = await fetch('http://127.0.0.1:8000/reset/password/', {
+        let response = await fetch('http://127.0.0.1:8000/account/reset/password/', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
@@ -57,10 +127,12 @@ export const AuthProvider = ({children}) => {
         })
         let data = await response.json()
         if (response.status === 200) {
-            alert('Please check your email')
-        } else {
-            console.log(response)
+            toast.success("Please check your email", {
+                position: toast.POSITION.TOP_RIGHT,
+                containerId: 'loginPage',
+            })
         }
+        history.push('/')
     }
 
 
@@ -68,10 +140,10 @@ export const AuthProvider = ({children}) => {
         e.preventDefault()
         let url = window.location.pathname
         let url_split = url.split('/')
-        let uid = url_split[3]
-        let token = url_split[4]
+        let uid = url_split[4]
+        let token = url_split[5]
 
-        let response = await fetch(`http://127.0.0.1:8000/set/password/${uid}/${token}`, {
+        let response = await fetch(`http://127.0.0.1:8000/account/set/password/${uid}/${token}/`, {
             method: 'PUT',
             headers: {
                 'Content-Type': 'application/json',
@@ -85,16 +157,24 @@ export const AuthProvider = ({children}) => {
         })
         let data = await response.json()
         if (response.status === 200) {
-            alert('Password has been changed.')
             history.push('/')
+            toast.success("Your password has been changed!", {
+                position: toast.POSITION.TOP_RIGHT,
+                containerId: 'loginPage',
+            })
         } else {
-            alert('Ups something gone wrong, try again.')
+            toast.error("Something gone wrong, try again later", {
+                position: toast.POSITION.TOP_CENTER,
+            })
+            setTimeout(function () {
+                window.location.replace('/reset/password');
+            }, 7000);
         }
     }
 
 
     let updateToken = async () => {
-        let response = await fetch('http://127.0.0.1:8000/api/token/refresh/', {
+        let response = await fetch('http://127.0.0.1:8000/account/api/token/refresh/', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
@@ -102,13 +182,15 @@ export const AuthProvider = ({children}) => {
             body: JSON.stringify({'refresh': authTokens?.refresh}) // if there's no authTokens, don't call refresh func
         })
         let data = await response.json()
-
         if (response.status === 200) {
-            console.log('Token Updated')
             setAuthTokens(data)
             setUser(jwt_decode(data.access))
             localStorage.setItem('authTokens', JSON.stringify(data))
         } else {
+            toast.warning("Sorry you need to logged in again", {
+                position: toast.POSITION.TOP_CENTER,
+                containerId: 'loginPage',
+            })
             logoutUser()
         }
 
@@ -123,10 +205,16 @@ export const AuthProvider = ({children}) => {
         setUser(null)
         localStorage.removeItem('authTokens')
         history.push('/login')
+        toast.warning("You are successfully logged out", {
+            position: toast.POSITION.TOP_RIGHT,
+            containerId: 'loginPage',
+        })
     }
 
 
     let contextData = {
+        activateAccount: activateAccount,
+        registerUser: registerUser,
         loginUser: loginUser,
         logoutUser: logoutUser,
         resetPasswordUser: resetPasswordUser,
