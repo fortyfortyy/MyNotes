3e38e0b4acba5339ebc17001dc6d614190bcad71
Daniel Pacek <d.pacek1@gmail.com>
2022-04-08 15:59:44 +0200

    created url path for recaptcha

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from api.views import NotesListView, NoteListView, NoteCreateView
+from api.views import NotesListView, NoteListView, NoteCreateView, recaptcha
 
 urlpatterns = [
     path('notes/', NotesListView.as_view(), name='notes-view'),
     path('notes/new/', NoteCreateView.as_view(), name='note-create-view'),
     path('notes/<uuid:note_uuid>/', NoteListView.as_view(), name='note-view'),
+    path('recaptcha/', recaptcha, name='recaptcha'),
 ]
