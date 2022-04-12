6472d15aff93a7f230fa4a963e4d9148b749cf85
Daniel Pacek <d.pacek1@gmail.com>
2022-04-12 19:48:37 +0200

    modified ALLOWED_HOSTS

--- mynotes/settings.py
+++ mynotes/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['www.mysimplenotes.app', '127.0.0.1:8000', 'localhost', '192.168.0.8']
+ALLOWED_HOSTS = ['*']
 PASSWORD_RESET_TIMEOUT = 120  # reset password token after 2 min
 
 # Application definition
