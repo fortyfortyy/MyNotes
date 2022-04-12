a42dabf7adff933c128678e96ab213377836987b
Daniel Pacek <d.pacek1@gmail.com>
2022-04-12 22:13:47 +0200

    created STATIC_ROOT

--- mynotes/settings.py
+++ mynotes/settings.py
@@ -198,7 +198,7 @@ STATICFILES_DIRS = [
     BASE_DIR / 'build/static',
 ]
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
