118e3fbb9a80c33d5ce2411c33c33e5d10648da6
Daniel Pacek <d.pacek1@gmail.com>
2022-04-12 22:49:35 +0200

    change static url

--- mynotes/settings.py
+++ mynotes/settings.py
@@ -197,7 +197,7 @@ EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'build/static',
 ]
