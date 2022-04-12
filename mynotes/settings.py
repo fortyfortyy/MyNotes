dae57aa781f5b9315892bb2abed8d72ab6ad9ec4
Daniel Pacek <d.pacek1@gmail.com>
2022-04-12 20:46:53 +0200

    changed secure proxy ssl header

--- mynotes/settings.py
+++ mynotes/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['www.mysimplenotes.app', 'localhost', '192.168.0.8', '127.0.0.1', 'mysimplenotes.app']
 PASSWORD_RESET_TIMEOUT = 120  # reset password token after 2 min
 
 # Application definition
@@ -217,8 +217,8 @@ AUTHENTICATION_BACKENDS = ['django.contrib.auth.backends.AllowAllUsersModelBacke
 
 RECAPTCHA = env('RECAPTCHA')
 
-# if not DEBUG:
-    # SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-    # SECURE_SSL_REDIRECT = True
+if not DEBUG:
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+    SECURE_SSL_REDIRECT = True
 
 django_heroku.settings(locals())
