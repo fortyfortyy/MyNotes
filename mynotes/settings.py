43ee8009de0823a5f4c54615d21baa2c468fd268
Daniel Pacek <d.pacek1@gmail.com>
2022-04-16 15:25:08 +0200

    cleaning up

--- mynotes/settings.py
+++ mynotes/settings.py
@@ -230,13 +230,20 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ORIGIN_WHITELIST = (
-    'localhost:3000',
-)
-
+CORS_ALLOW_ALL_ORIGINS = True
 CSRF_COOKIE_NAME = "csrftoken"
-
 WHITENOISE_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_HEADERS = [
+    "accept",
+    "accept-encoding",
+    "authorization",
+    "content-type",
+    "dnt",
+    "origin",
+    "user-agent",
+    "x-csrftoken",
+    "x-requested-with",
+]
 
 AUTH_USER_MODEL = 'users.ProfileUser'
 SITE_ID = 1
