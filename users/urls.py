668f69ff786b5a9b0a684c6a48eae5c89cca35e6
Daniel Pacek <d.pacek1@gmail.com>
2022-04-16 14:59:19 +0200

    added prefix api/ to paths

--- users/urls.py
+++ users/urls.py
@@ -1,13 +1,13 @@
-from django.urls import path
-from rest_framework_simplejwt.views import TokenRefreshView
-
-from users.views import ForgottenPasswordView, ChangePasswordView, CustomTokenObtainPairView, RegisterUserView, ActivateAccountView
+from django.urls import path, re_path
+from users.views import ForgottenPasswordView, ChangePasswordView, CustomTokenObtainPairView, RegisterUserView, \
+    ActivateAccountView, CustomTokenRefreshView
 
 urlpatterns = [
     path('api/token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/refresh/', CustomTokenRefreshView.as_view(), name='token_refresh'),
     path('api/register/', RegisterUserView.as_view(), name='register'),
-    path('activate/<uidb64>/<token>/', ActivateAccountView.as_view(), name='activate_account'),
+    re_path(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+            ActivateAccountView.as_view(), name='activate_account'),
 
     path('reset/password/', ForgottenPasswordView.as_view(), name='forgotten_password'),
     path('set/password/<uidb64>/<token>/', ChangePasswordView.as_view(), name='password_reset_confirm'),
