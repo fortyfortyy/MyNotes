899ca9a52e47c5cf4949672f8a47368e84a97aaa
Daniel Pacek <d.pacek1@gmail.com>
2022-04-16 15:29:57 +0200

    cleaning up

--- mynotes/settings.py
+++ mynotes/settings.py
@@ -21,8 +21,8 @@ import mimetypes
 
 import environ
 
-# mimetypes.add_type("text/javascript", ".js", True)
-# mimetypes.add_type("text/css", ".css", True)
+mimetypes.add_type("text/javascript", ".js", True)
+mimetypes.add_type("text/css", ".css", True)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
