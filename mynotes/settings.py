aac08b6deb50392432c00140a9b2b31fc745ddeb
Daniel Pacek <d.pacek1@gmail.com>
2022-04-18 19:17:10 +0200

    cleaning up for deploy

--- mynotes/settings.py
+++ mynotes/settings.py
@@ -18,6 +18,9 @@ from django.conf import settings
 import django_heroku
 import dj_database_url
 import environ
+import mimetypes
+
+mimetypes.add_type("text/css", ".css", True)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
