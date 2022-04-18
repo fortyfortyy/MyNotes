90e1d01cf323b42f414cdfe88afc798af6907306
Daniel Pacek <d.pacek1@gmail.com>
2022-04-18 19:35:59 +0200

    cleaning up for deploy

--- mynotes/settings.py
+++ mynotes/settings.py
@@ -209,12 +209,11 @@ EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'build/static'),
     # BASE_DIR / 'build/static',
 ]
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 CORS_ALLOW_ALL_ORIGINS = True
